23fcc2dac2b2b996a0f2062131a1597e0f72ae49
Sviatlana29 <sviatlana.astapovich@gmail.com>
2022-10-12 11:38:28 -0500

    FW folder

---
+++ FW/src/app.js
@@ -0,0 +1,9 @@
+// 345 - 543
+// -234 -> -432
+
+function convert(num){
+    let str = "" + Math.abs(num)
+    return Math.sign(num) * (+str.split('').reverse().join(''))
+}
+
+module.exports = convert;
