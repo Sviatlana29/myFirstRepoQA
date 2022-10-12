9c86bf4cfe771a407c0c21fd20977b69e51292fa
Sviatlana29 <sviatlana.astapovich@gmail.com>
2022-10-12 11:26:55 -0500

    2 Commit  - Html folder

---
+++ html/mycode.js
@@ -0,0 +1,109 @@
+// data: cup, coffee, water, milk, spoon
+// water: hot, 200 ml
+// spoon: tea spoon
+// coffee: ground
+// cup : 250 ml
+// milk: 20 ml
+
+//  Steps:
+// 1. Take a cup
+// 2. Open coffee box
+// 3. Take a spoon
+// 4. Take coffee from the coffee box with a spoon
+// 5. Put coffee in a cup 
+// 6. Pour water in a cup with coffee
+// 7. Pour milk  in a cup with coffee
+// 8. Take a sugar with a spoon
+// 9. Put sugar to a  cup with coffee
+// 10. Stir coffee in a cup with a spoon 
+
+let coffee, cupSize;
+let water = "hot water";
+let drink = 'hot milk';
+let sweet = 'sugar';
+let price = 3;
+let smallCup = "small cup"
+let mediumCup = "medium cup";
+let largeCup = "large cup";
+
+let latteMacchiato = 'Latte Macchiato';
+let cappucchino = "Cappucchino";
+let americano = 'Americano';
+
+
+coffee = cappucchino;
+cupSize = smallCup;
+let typeOfCoffeeSelected = true;
+let sizeOfCoffeeSelected = true;
+
+if (typeOfCoffeeSelected == true && sizeOfCoffeeSelected == true ) {
+
+if (coffee == cappucchino) {
+  price *=1.5
+}
+else if(coffee  == latteMacchiato) {
+  let changeDrink
+  changeDrink =  water;
+  water = drink;
+  drink = changeDrink;
+  price *=2
+} 
+else if (coffee == americano) {
+  let changeDrink
+  drink = changeDrink = 'a lit bit more hot water';
+}
+// else {
+//   alert("You did not choose a type of coffee.")
+//    document.write("You did not choose a type of coffee." + "<br>")
+//   console.log("You did not choose a type of coffee.", "\n")
+// };
+
+ if (cupSize == mediumCup) {
+  price *= 1.5
+} 
+  else if (cupSize == largeCup)  {
+  price *= 2
+}
+else if 
+  (cupSize == smallCup) {
+  price 
+  }
+// else {
+//   alert("You did not choose a size of coffee.")
+//    document.write("You did not choose a size of coffee." + "<br>")
+//         console.log("You did not choose a size of coffee.", "\n")
+// };
+
+let nameCoffee = `Make a ${cupSize} of ${coffee}:`;
+let step1 = `1. Take a ${cupSize}.`;
+let step2 = "2. Open a coffee box.";
+let step3 = "3. Take a spoon.";
+let step4 = "4. Take coffee from the coffee box with spoon.";
+let step5 = `5. Put coffee in a ${cupSize}.`;
+let step6 = `6. Pour ${water} in a cup with coffee.`;
+let step7 = `7. Pour ${drink}  in a cup with coffee.`;
+let step8 = `8. Take a ${sweet} with a spoon.`;
+let step9 = `9. Put ${sweet} to a cup with coffee.`;
+let step10 = "10. Stir coffee in a cup with a spoon.";
+const resultCoffee = `A ${cupSize} of ${coffee} costs $${price}! \n Enjoy your ${coffee}!`;
+
+  document.write(nameCoffee + "<br>" + step1 + "<br>" +  step2 + "<br>" + step3 + "<br>" +  step4 + "<br>" + step5 + "<br>" +  step6 + "<br>" +  step7 + "<br>" + step8 + "<br>" +  step9 + "<br>" + step10 + "<br>" + resultCoffee + "<br>")
+  
+console.log(nameCoffee, "\n", step1, "\n", step2, "\n", step3 , "\n",   step4, "\n", step5, "\n", step6, "\n", step7, "\n", step8, "\n", step9 , "\n", step10, "\n", resultCoffee)
+}
+  
+else if (typeOfCoffeeSelected == false) {
+  // alert("You did not choose a type  of coffee.")
+    document.write("You did not choose a type or size of coffee." + "<br>")
+  console.log("You did not choose a type or size of coffee.", "\n")
+}
+  else if (sizeOfCoffeeSelected == false) {
+  // alert("You did not choose a size of coffee.")
+    document.write("You did not choose a type or size of coffee." + "<br>")
+  console.log("You did not choose a type or size of coffee.", "\n")
+}
+else {
+  // alert("Please choose type or size of coffee.")
+    document.write("Please choose type or size of coffee." + "<br>")
+  console.log("Please choose type or size of coffee.", "\n")
+}
